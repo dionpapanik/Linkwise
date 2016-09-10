d7fce1dd6bbbae1052d5cf0e2441ffe93c03e71e
dpapanikolaou <dionysis_172@hotmail.com>
2016-09-10 15:41:08 +0300

    fetch correct category data
    fetch the products only for one cagetory page based on the pagination
    and user preferances

--- app/code/local/DigitalUp/Linkwise/Block/Lnkws.php
+++ app/code/local/DigitalUp/Linkwise/Block/Lnkws.php
@@ -3,6 +3,13 @@
 class DigitalUp_Linkwise_Block_Lnkws extends Mage_Core_Block_Template
 {
 
+	/**
++     * identify the curent page based on the m/c/a
+	  * bulid the js
+	  *
++     * @return $params (js string)
++     */
+
     public function getLinkwiseData()
     {
         $helper = Mage::helper('linkwise');
@@ -14,12 +21,37 @@ class DigitalUp_Linkwise_Block_Lnkws extends Mage_Core_Block_Template
                 break;
 
             case 'catalog/category/view': //category page
+
+			    /**
++   		  	* return products not for whole category
++				* but for the exact page user is
++     			*/
+
+           		 // Varien_Profiler::start('catalog/category/view');
+
+                $currentPage = (int)Mage::App()->getRequest()->getParam('p'); // read the pagination of the user
+                $currentPage = (($currentPage == 0) ? 1 : $currentPage);
+
+                $limit = (int)Mage::App()->getRequest()->getParam('limit'); // read the limit of the user
+                $limit = (($limit == 0) ? 12 : $limit);
+
+                $dir = (string)Mage::App()->getRequest()->getParam('dir'); // read the direction of the user
+                $dir = (($dir == 'asc') ? 'asc' : 'desc');
+
+                $order = (string)Mage::App()->getRequest()->getParam('order'); // read the ordering of the user //position, price etc
+                $order = (($order == 0) ? 'position' : $order);
+
                 $category = Mage::registry('current_category');
                 $productCollection = Mage::getResourceModel('catalog/product_collection')
                     ->addCategoryFilter($category)
                     ->addAttributeToFilter('status', 1)
                     ->addAttributeToFilter('visibility', array('in' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH))
-                    ->addAttributeToSelect(array('sku', 'price', 'special_price'));
+                    ->addAttributeToSelect(array('sku', 'price', 'special_price', 'tax_class_id'))
+                    ->addAttributeToSort($order, $dir)
+                    ->setCurPage($currentPage)
+                    ->setPageSize($limit);
+
+                $productCollection->load();
 
                 empty($params);
                 $params = '';
@@ -31,6 +63,8 @@ class DigitalUp_Linkwise_Block_Lnkws extends Mage_Core_Block_Template
                         '});';
                 }
                 $params .= PHP_EOL . 'lw("listItems");';
+
+                // Varien_Profiler::stop('catalog/category/view');
                 break;
 
             case 'catalog/product/view': // product page
@@ -71,7 +105,7 @@ class DigitalUp_Linkwise_Block_Lnkws extends Mage_Core_Block_Template
                 $params = 'lw("checkout");';
                 break;
 
-            case 'onestepcheckout/index/index': // lotusbreath onestepcheckout module!!
+            case 'onestepcheckout/index/index': // onestepcheckout module!!
                 empty($params);
                 $params = 'lw("checkout");';
                 break;
